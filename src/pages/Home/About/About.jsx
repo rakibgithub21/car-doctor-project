4cddf5432c4c4ed2ddcf70772704ae0caff39c5b
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-01 08:02:06 +0600

    finsihed about section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
 
 const About = () => {
     return (
-        <div className="hero mb-8 min-h-screen bg-base-200">
+        <div className="hero mb-8 min-h-[60vh] rounded-lg bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className='lg:w-1/2 relative'>
+                    <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                    <img className='w-1/2 right-5 top-1/2 absolute rounded-lg border-8 border-white' src={parts} alt="" />
+                </div>
+                <div className='lg:w-1/2 md:mt-10 lg:mt-0 '>
+                    <p className='text-2xl font-medium text-orange-500 '>About Us</p>
+                    <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.  </p>
+                    <button className="btn btn-warning ">Get More Info</button>
                 </div>
             </div>
         </div>
