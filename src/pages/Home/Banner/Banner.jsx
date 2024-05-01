ba275f5807913bb8c527e9e4c0017d5495074f3e
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-01 07:36:34 +0600

    finsihed slider

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,36 +1,80 @@
-
+import image1 from '../../../assets/images/banner/1.jpg'
+import image2 from '../../../assets/images/banner/2.jpg'
+import image3 from '../../../assets/images/banner/3.jpg'
+import image4 from '../../../assets/images/banner/4.jpg'
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel h-[80vh] w-full my-8">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                <img src={image1} className="w-full  rounded-lg" />
+                <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-lg   ">
+                    <div className='text-white pl-12 w-2/3 space-y-7'>
+                        <h2 className='text-6xl font-medium'>Affordable Price For Car Servicing</h2>
+                        <p>There Are Many Variations Of Passage Of Available, But The Majority Have Suffered Alteration In Some Form</p>
+                        <div className='flex gap-10'>
+                            <button className="btn btn-primary">Discover More</button> 
+                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
+                <img src={image2} className="w-full" />
+                <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-lg   ">
+                    <div className='text-white pl-12 w-2/3 space-y-7'>
+                        <h2 className='text-6xl font-medium'>Affordable Price For Car Servicing</h2>
+                        <p>There Are Many Variations Of Passage Of Available, But The Majority Have Suffered Alteration In Some Form</p>
+                        <div className='flex gap-10'>
+                            <button className="btn btn-primary">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
+                <img src={image3} className="w-full rounded-lg" />
+                <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-lg   ">
+                    <div className='text-white pl-12 w-2/3 space-y-7'>
+                        <h2 className='text-6xl font-medium'>Affordable Price For Car Servicing</h2>
+                        <p>There Are Many Variations Of Passage Of Available, But The Majority Have Suffered Alteration In Some Form</p>
+                        <div className='flex gap-10'>
+                            <button className="btn btn-primary">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
+                <img src={image4} className="w-full rounded-lg" />
+                <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-lg   ">
+                    <div className='text-white pl-12 w-2/3 space-y-7'>
+                        <h2 className='text-6xl font-medium'>Affordable Price For Car Servicing</h2>
+                        <p>There Are Many Variations Of Passage Of Available, But The Majority Have Suffered Alteration In Some Form</p>
+                        <div className='flex gap-10'>
+                            <button className="btn btn-primary">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>
+            
         </div>
     );
 };
