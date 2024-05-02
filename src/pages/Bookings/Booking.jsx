172063889a38572ecf5364a44b035f44001395bc
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-02 16:13:13 +0600

    finsihed update bookings

--- src/pages/Bookings/Booking.jsx
+++ src/pages/Bookings/Booking.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 import BookingRow from "./BookingRow";
+import { CgLayoutGrid } from "react-icons/cg";
 
 
 const Booking = () => {
@@ -13,6 +14,44 @@ const Booking = () => {
             .then(res => res.json())
             .then(data => setBookings(data))
     }, [url])
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure')
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = bookings.filter(booking => booking._id !== id)
+                    setBookings(remaining)
+
+                })
+        }
+    }
+
+    const handleBookingConfirm = (id) => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'Confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id)
+                    updated.status = 'Confirm'
+                    const newBookings = [updated, ...remaining]
+                    setBookings(newBookings)
+                }
+            })
+    }
     return (
         <div className="overflow-x-auto">
             <table className="table">
@@ -20,27 +59,27 @@ const Booking = () => {
                 <thead>
                     <tr>
                         <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
+
                         </th>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                        <th></th>
+                        <th>Image</th>
+                        <th>Service</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         bookings.map(booking => <BookingRow
-                        
+                            handleDelete={handleDelete}
                             key={booking._id}
                             booking={booking}
+                            handleBookingConfirm={handleBookingConfirm}
                         ></BookingRow>)
                     }
-                    
+
                 </tbody>
-                
+
 
             </table>
         </div>
--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,13 +1,14 @@
 
 
-const BookingRow = ({ booking }) => {
-    const { img, title, date, service, price } = booking
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+    const { _id, img, title, date, service, price,status } = booking
+    
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td><div className="avatar">
                 <div className="mask mask-squircle w-24 rounded h-24">
@@ -21,7 +22,9 @@ const BookingRow = ({ booking }) => {
             <td>{date}</td>
             <td>{price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                {
+                    status === 'Confirm' ? <span className="font-bold text-primary">confirmed</span> : <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirmed</button>
+               }
             </th>
         </tr>
     );
